5ee3a4d993350666a3d66694bdadd2e17295c6a4
Zoquinha123 <gilson.morais@fatec.sp.gov.br>
2022-03-21 17:49:53 +0000

    gravando

---
+++ Matriz.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Matriz{
+ public static void main(String[] args) {
+
+    int [] idades;
+    idades=new int[5];
+    Scanner sc=new Scanner(System.in);
+    for (int index = 0; index < idades.length; index++) {
+        System.out.println("Digite o "+(index+1)+ "numero");
+        idades[index]=sc.nextInt();
+     
+        System.out.println("eu tenho "+idades[index]+" anos");
+    }  
+
+}
+
+}
---
+++ Tabuada.java
@@ -0,0 +1,14 @@
+public class Tabuada {
+    public static void main(String[] args) {
+
+        int idades[] = new int [11];
+
+        for(int i=0; i < idades.length; i++) {
+            idades[i] = i*10;
+        }
+
+        for(int i:idades) {
+            System.out.println(i);
+        }
+    }
+}
